b5e1ff584d51b28027da2e6a78be76ab5e5377f6
gsuStudent <35585938+gsuStudent@users.noreply.github.com>
2021-02-05 10:07:41 -0500

    Sprint combine modal and calendar (#44)
    * Update calendar.tsx
    
    * Create work calendar screen
    
    add skeleton for work calendar
    
    * update package json
    
    update lib for date dns
    
    * calendar skeleton
    
    Added initial work calendar
    
    * update pages\calendar.tsx
    
    update pages\calendar.tsx to fix push error
    
    * Update index.tsx
    
    readd calendar skeleton
    
    * update css
    
    update css to reflect more of the prototype design
    
    * added headers/fixed daysOfWeek
    
    added Work Calendar header
    fixed month toggle
    fixed days of the week bar
    
    * updated calendar numbers
    
    * adjust calendar header
    
    remove column application from header arrows and month: still need to fix spacing of arrows and center month
    
    * add mock up of shift modals
    
    added inital modals for shifts for work calendar
    
    * add css
    
    add some styling to modals
    
    * update Calendar.module.css
    
    updated layout for main calendar page
    
    * add mock data and connect edit shift
    
    connect edit shift to calendar and mock data
    
    * add css to modal pop ups
    
    add css styling changes
    
    * remove unused imports
    
    remove unused variables
    
    * remove wrap from modal title
    
    force tile of modals to use max width
    
    * Fix linting errors
    
    * Remove unnecessary pages
    
    * update Calendar.module.css
    
    fixed days bar width & calendar width ~ should resize properly with screen & days are now aligned over columns
    
    * update Calendar.module.css
    
    add cell number hover icon
    
    * Update Calendar.module.css
    
    fix screen sizing resolution issue
    
    * Fix z-index for responsive navbar
    
    Co-authored-by: Lake Miller <lakemiller18@gatech.edu>
    Co-authored-by: ishan baniya <ibaniya98@gmail.com>

---
+++ components/shift-main-add-modal/index.tsx
@@ -0,0 +1,73 @@
+import React from "react"
+import { Button, Col, Modal, Row } from "react-bootstrap"
+import styles from "styles/ShiftModal.module.css"
+import ShiftAddModal from "../shift-add-modal"
+
+interface ShiftMainAddModalProps {
+  date: Date
+  hourly?: string
+  show: boolean
+  onHide: (boolean) => void
+}
+
+const ShiftMainAddModal: React.FunctionComponent<ShiftMainAddModalProps> = (
+  props
+) => {
+  const [showWageModel, setWageModel] = React.useState({
+    wageType: "",
+    wageMode: false,
+  })
+
+  const openHourlyWageModal = () => {
+    props.onHide(false)
+    setWageModel({ wageType: "hourly", wageMode: true })
+  }
+
+  const openNonHourlyWageModal = () => {
+    props.onHide(false)
+    setWageModel({ wageType: "nonHourly", wageMode: true })
+  }
+
+  return !props.show && showWageModel.wageMode ? (
+    <ShiftAddModal
+      date={props.date}
+      show={showWageModel.wageMode}
+      hourly={showWageModel.wageType}
+      onHide={setWageModel}
+    />
+  ) : (
+    <Modal
+      {...props}
+      size="lg"
+      contentClassName={styles.modelStyle}
+      backdrop="static"
+    >
+      <Modal.Header closeButton>
+        <Modal.Title className={styles.mainTitle}>
+          Select Your Wage Type
+        </Modal.Title>
+      </Modal.Header>
+      <Modal.Body>
+        <Row className={styles.shiftMainModal}>
+          <Col xs={5}>
+            <Button className={styles.mainButton} onClick={openHourlyWageModal}>
+              Hourly Wages
+            </Button>
+          </Col>
+
+          <Col xs={5}>
+            <Button
+              className={styles.mainButton}
+              onClick={openNonHourlyWageModal}
+            >
+              Non-Hourly Wages
+            </Button>
+          </Col>
+        </Row>
+      </Modal.Body>
+      <Modal.Footer></Modal.Footer>
+    </Modal>
+  )
+}
+
+export default ShiftMainAddModal
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,6 +2,7 @@ import "bootstrap/dist/css/bootstrap.min.css"
 import "../styles/globals.css"
 
 import { AppProps } from "next/app"
+import "bootstrap/dist/css/bootstrap.min.css"
 
 const MyApp: React.FunctionComponent<AppProps> = ({ Component, pageProps }) => {
   return <Component {...pageProps} />
