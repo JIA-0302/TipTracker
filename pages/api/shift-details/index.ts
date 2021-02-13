9da6df476f72779b3569f62fe3f7d1e6fd2182ad
ishan baniya <ibaniya98@gmail.com>
2021-02-13 16:22:06 -0500

    Add starter code for shift details API

---
+++ pages/api/shift-details/index.ts
@@ -0,0 +1,20 @@
+import type { NextApiHandler, NextApiRequest, NextApiResponse } from "next"
+import { getShiftDetailsByUserId } from "server/mysql/actions/shiftData"
+
+const handler: NextApiHandler = async (
+  req: NextApiRequest,
+  res: NextApiResponse
+) => {
+  if (req.method === "GET") {
+    try {
+      const userId = 2 // TODO - add middleware to extract user id after authentication is implemented
+      const results = await getShiftDetailsByUserId(userId)
+
+      res.status(200).json(results)
+    } catch (e) {
+      res.status(500).json({ message: e.message })
+    }
+  }
+}
+
+export default handler
---
+++ server/mysql/models/user.ts
@@ -0,0 +1,8 @@
+export default interface IUser {
+  user_id?: number
+  first_name: string
+  last_name: string
+  email: string
+  timezone: string
+  email_verified?: boolean
+}
