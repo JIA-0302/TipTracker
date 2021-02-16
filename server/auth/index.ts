2a6d385eee818ccc228b22c7e143d66d4d2c53ed
ishan baniya <ibaniya98@gmail.com>
2021-02-16 15:31:29 -0500

    Add registration API endpoint with validations

--- server/auth/index.ts
+++ server/auth/index.ts
@@ -1,14 +1,35 @@
 import bcrypt from "bcrypt";
 
-export async function isValidPassword(
-  password,
-  passwordHash
+export async function matchesPasswordHash(
+  password: string,
+  passwordHash: string
 ): Promise<boolean> {
   return await bcrypt.compare(password, passwordHash);
 }
 
-export async function getPasswordHash(password): Promise<string> {
-  const salt = await bcrypt.genSaltSync(process.env.BYCRYPT_SALT_ROUNDS);
+export async function getPasswordHash(password: string): Promise<string> {
+  const saltRounds = Number(process.env.BYCRYPT_SALT_ROUNDS);
+  const salt = await bcrypt.genSaltSync(saltRounds);
   const passwordHash = await bcrypt.hashSync(password, salt);
   return passwordHash;
+}
+
+/**
+ * Check if the new registering user's password is valid.
+ * Currently it only checks if the length >= 8
+ *
+ * @param password New password entered by the user
+ */
+export function isValidPassword(password: string): boolean {
+  return password.trim().length >= 8;
+}
+
+/**
+ * Uses regular expression to check if the email is valid
+ *
+ * @param email Email provided by the user
+ */
+export function isValidEmail(email: string): boolean {
+  const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  return re.test(String(email).toLowerCase());
 }
