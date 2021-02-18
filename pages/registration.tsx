038912fe86ba7b57dd2549464ae94913b3ce3584
gsuStudent <35585938+gsuStudent@users.noreply.github.com>
2021-02-17 19:27:41 -0500

    initial registration (#50)
    * initial registration
    
    added registration page
    
    * add auth layout
    
    added separate layout for signin and and removed id field
    
    Co-authored-by: iBaniya <ibaniya98@gmail.com>

---
+++ pages/registration.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Registration from "../components/registration";
+import AuthLayout from "../components/layouts/auth-layout";
+
+const Home: React.FunctionComponent = () => {
+    return (
+        <AuthLayout>
+            <Registration/>
+        </AuthLayout>
+    );
+};
+export default Home;
