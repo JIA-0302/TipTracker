d8b2695bc6adcaf7bbb6e1a184fd0ac0c8be6fb7
gsuStudent <35585938+gsuStudent@users.noreply.github.com>
2021-02-25 10:05:22 -0500

    Sprint2 add register api to register page (#58)
    * add fetch to register page
    
    add api route to register page
    
    * switch to react form
    
    switched from react bootstrap form to react hook form, form does client side validation before calling api
    
    * remove log
    
    * remove unused varable
    
    * Run prettier
    
    Co-authored-by: ishan baniya <ibaniya98@gmail.com>

--- components/layouts/auth-layout/index.tsx
+++ components/layouts/auth-layout/index.tsx
@@ -4,32 +4,32 @@ import { Row, Col } from "react-bootstrap";
 import styles from "../../../styles/Home.module.css";
 
 const AuthLayout: React.FunctionComponent = ({ children }) => {
-    return (
-        <div className={styles.container}>
-            <Head>
-                <title>TipTracker</title>
-                <link rel="icon" href="/favicon.ico" />
-            </Head>
+  return (
+    <div className={styles.container}>
+      <Head>
+        <title>TipTracker</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
 
-            <Row>
-                <Col xs={12} md={5}>
-                    <Image
-                        src="/images/tip-tracker-logo.png"
-                        alt="Logo of TipTracker Application"
-                        width={400}
-                        height={400}
-                    />
-                </Col>
-                <Col xs={12} md={7}>
-                    {children}
-                </Col>
-            </Row>
+      <Row>
+        <Col xs={12} md={5}>
+          <Image
+            src="/images/tip-tracker-logo.png"
+            alt="Logo of TipTracker Application"
+            width={400}
+            height={400}
+          />
+        </Col>
+        <Col xs={12} md={7}>
+          {children}
+        </Col>
+      </Row>
 
-            {/* <footer className={styles.footer}>
+      {/* <footer className={styles.footer}>
 
       </footer> */}
-        </div>
-    );
+    </div>
+  );
 };
 
 export default AuthLayout;
