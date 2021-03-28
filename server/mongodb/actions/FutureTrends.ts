38902835a798b8493440cd943c2f12191e697333
gsuStudent <cyberbot300@gmail.com>
2021-03-28 16:03:45 -0400

    add api route for future shifts
    added route to get weekly future shifts,
    currently returns mock data

--- server/mongodb/actions/FutureTrends.ts
+++ server/mongodb/actions/FutureTrends.ts
@@ -53,3 +53,73 @@ export async function deleteFutureTrendData(shiftDataHash: string) {
     hash: shiftDataHash,
   });
 }
+
+
+export async function getWeeklyFutureTrendsData(
+    userId: number,
+    start_date: string,
+    end_date: string
+) {
+ // await mongoDB();
+
+  const futureShiftDetails = [
+    {
+      "userId" : 1,
+      "shift_date":"2021-03-28",
+      "start_time":"2021-03-28T10:14:01.054Z",
+      "end_time":"2021-03-28T16:18:01.054Z",
+      "cash_tips": 100,
+      "credit_card_tips": 50
+    },
+    {
+      "userId" : 1,
+      "shift_date":"2021-03-29",
+      "start_time":"2021-03-29T16:04:01.054Z",
+      "end_time":"2021-03-29T21:10:01.054Z",
+      "cash_tips": 250,
+      "credit_card_tips": 49
+    },
+    {
+      "userId" : 1,
+      "shift_date":"2021-03-30",
+      "start_time":"2021-03-30T10:14:01.054Z",
+      "end_time":"2021-03-30T16:18:01.054Z",
+      "cash_tips": 50,
+      "credit_card_tips": 200
+    },
+    {
+      "userId" : 1,
+      "shift_date":"2021-03-31",
+      "start_time":"2021-03-31T16:10:01.054Z",
+      "end_time":"2021-03-28T19:18:01.054Z",
+      "cash_tips": 117,
+      "credit_card_tips": 35
+    },
+    {
+      "userId" : 1,
+      "shift_date":"2021-04-01",
+      "start_time":"2021-04-01T12:13:01.054Z",
+      "end_time":"2021-04-01T21:21:01.054Z",
+      "cash_tips": 121,
+      "credit_card_tips": 23
+    },
+    {
+      "userId" : 1,
+      "shift_date":"2021-04-02",
+      "start_time":"2021-04-02T05:14:01.054Z",
+      "end_time":"2021-03-28T16:13:01.054Z",
+      "cash_tips": 323,
+      "credit_card_tips": 12
+    },
+    {
+      "userId" : 1,
+      "shift_date":"2021-04-03",
+      "start_time":"2021-04-03T16:14:01.054Z",
+      "end_time":"2021-04-03T20:18:01.054Z",
+      "cash_tips": 110,
+      "credit_card_tips": 66
+    }
+  ];
+
+  return futureShiftDetails;
+}
