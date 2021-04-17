e60f71bdb4bdfdb2b889896f3708ec754a2e6f4e
Ishan Baniya <ibaniya98@gmail.com>
2021-04-17 12:55:42 -0400

    Fix date parsing for analytics page

--- components/visualizations/monthly-summary/wagesDistribution.tsx
+++ components/visualizations/monthly-summary/wagesDistribution.tsx
@@ -35,7 +35,7 @@ const WagesDistribution = (): JSX.Element => {
           <ResponsivePie
             data={earningsData}
             valueFormat={(v) => `$ ${v}`}
-            margin={{ top: 40, right: 80, bottom: 80, left: 80 }}
+            margin={{ top: 40, right: 80, bottom: 80, left: 100 }}
             innerRadius={0.5}
             padAngle={0.7}
             cornerRadius={3}
