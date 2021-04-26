d17ddd9731a35355ba7593a01a9c7585b4023273
Ishan Baniya <ibaniya98@gmail.com>
2021-04-26 02:22:38 -0400

    Update user profile component design

--- components/user-details/index.tsx
+++ components/user-details/index.tsx
@@ -1,18 +1,8 @@
 import React from "react";
 import styles from "../user-details/user-details.module.css";
 import Image from "next/image";
-import {
-  List,
-  ListItem,
-  ListItemText,
-  ListItemAvatar,
-  Avatar,
-} from "@material-ui/core/";
-import AccountBoxIcon from "@material-ui/icons/AccountBox";
-import EmailIcon from "@material-ui/icons/Email";
-import EventIcon from "@material-ui/icons/Event";
-import ModalController from "components/work-schedule/";
-import UserProfile from "components/user-profile/";
+import WorkScheduleModalController from "components/work-schedule/";
+import UserProfileModalController from "components/user-profile/";
 
 export interface UserDetailsProp {
   name: string;
@@ -20,47 +10,24 @@ export interface UserDetailsProp {
   image: string;
 }
 
-const UserDetails: React.FunctionComponent<UserDetailsProp> = (props) => {
+const UserDetails = ({ name, email, image }: UserDetailsProp): JSX.Element => {
   return (
     <div className={styles.div}>
-      <List>
-        <div className={styles.avatarContainer}>
-          <Image
-            src={props.image}
-            alt="Picture of the user"
-            width={135}
-            height={135}
-            className={styles.avatar}
-          />
-        </div>
-        <div className={styles.avatarContainer}>
-          <UserProfile></UserProfile>
-        </div>
-        <ListItem className={styles.listItem}>
-          <ListItemAvatar>
-            <Avatar>
-              <AccountBoxIcon className={styles.icon} />
-            </Avatar>
-          </ListItemAvatar>
-          <ListItemText primary={props.name} />
-        </ListItem>
-        <ListItem className={styles.listItem}>
-          <ListItemAvatar>
-            <Avatar>
-              <EmailIcon className={styles.icon} />
-            </Avatar>
-          </ListItemAvatar>
-          <ListItemText primary={props.email} />
-        </ListItem>
-        <ListItem className={styles.listItem}>
-          <ListItemAvatar>
-            <Avatar>
-              <EventIcon className={styles.icon} />
-            </Avatar>
-          </ListItemAvatar>
-          <ModalController></ModalController>
-        </ListItem>
-      </List>
+      <div className={styles.avatarContainer}>
+        <Image
+          src={image}
+          alt="Picture of the user"
+          width={135}
+          height={135}
+          className={styles.avatar}
+        />
+      </div>
+
+      <h3 className="mt-3">{name}</h3>
+      <p>{email}</p>
+
+      <UserProfileModalController />
+      <WorkScheduleModalController />
     </div>
   );
 };
